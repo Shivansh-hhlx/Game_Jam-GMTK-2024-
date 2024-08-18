385ef0b50e7d8037250a16dda00784fc63ebbc38
Shivansh <shivansh.saraswat.2304@gmail.com>
2024-08-18 18:38:23 +0530

    Snake animations added

--- enemy/enemy_snake.gd
+++ enemy/enemy_snake.gd
@@ -3,28 +3,47 @@ extends CharacterBody2D
 var speed = 50
 var player = false
 var pbody
+var falling = 1
+var fallen = -1
 
+func _ready():
+	$AnimatedSprite2D.global_position.y += 40
 
 func _process(delta):
 	if is_on_floor():
+		if fallen == 0:
+			$AnimatedSprite2D.play("fallen")
 		velocity.y = 0
 	else:
+		fallen = 0
+		$AnimatedSprite2D.play("falling")
+		if falling == 1:
+			falling = 0
+			$AnimatedSprite2D.flip_h = true
+		else:
+			falling = 1
+			$AnimatedSprite2D.flip_h = false
 		velocity.y += 1000 * delta
 		
+	
+	
 	move_and_slide()
+	
+	if fallen == 1:
+		$AnimatedSprite2D.play("default")
 	
 	if player:
 		if pbody.global_position.x > global_position.x:
 			$AnimatedSprite2D.flip_h = !false
 			velocity.x = 50
-			$AnimatedSprite2D.play("default")
 		else:
 			$AnimatedSprite2D.flip_h = !true
 			velocity.x = -50
-			$AnimatedSprite2D.play("default")
 	else:
 		$AnimatedSprite2D.stop()
 		velocity.x = 0
+	
+	
 	
 	
 
@@ -37,3 +56,8 @@ func _on_area_2d_body_entered(body):
 
 func _on_area_2d_body_exited(body):
 	player = false
+
+
+func _on_animated_sprite_2d_animation_finished():
+	fallen += 1
+	$AnimatedSprite2D.global_position.y -= 40
