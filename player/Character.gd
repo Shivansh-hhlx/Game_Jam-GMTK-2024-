d4de4b9169173d18e0e05e66d936382c9422d3bd
Aakash Bolla <abyssallaughter23@gmail.com>
2024-08-19 11:26:32 +0530

    Character size changes

--- player/Character.gd
+++ player/Character.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 var kill := preload("res://zones/enemy_kill_zone.tscn")
 
+var canScale: bool = true
+var currentScale: bool = false; # true for big, false for small
+var scaleFactor = 1;
+var scaleCount: int = 0;
+
 const RUNNING_SPEED = 400
 const WALKING_SPEED = 150
 const GRAVITY = 1000
@@ -18,6 +23,8 @@ var health = 100
 var strength = 10
 var stamina = 50
 var poise = 10
+
+var time_count = -1
 
 var killzone
 
@@ -77,18 +84,53 @@ func _process(delta):
 		is_stomping = 1
 		$Timer.start()
 		$Timer2.start()
-	
-	
-	move_and_slide()	
-	
+		
+	if Input.is_action_just_pressed("scale") and canScale:
+		print("boy")
+		if currentScale:
+			print("scaling down");
+			currentScale = false;
+			scaleFactor = -1;
+			
+			$Timer3.start()
+		else:
+			print("scaling up");
+			currentScale = true;
+			scaleFactor = 1;
+		
+			$Timer3.start()
+		
+	move_and_slide()
+
 
 func _on_timer_timeout():
 	gravity = GRAVITY
 	speed = WALKING_SPEED
 	jump = JUMP_SPEED
 
-
 func _on_timer_2_timeout():
 	is_dashing = 0
 	is_stomping = 0
 	remove_child(killzone)
+
+func _on_timer_3_timeout():
+	
+	scale.x += 0.5 * scaleFactor;
+	scale.y += 0.5 * scaleFactor;
+	if currentScale:
+		$Markers/Marker2D4.position.y += 25.0
+		$Markers/Marker2D2.position.y += 25.0
+		$Markers/Marker2D.position.y +=  25.0
+	else:
+		$Markers/Marker2D4.position.y -= 25.0
+		$Markers/Marker2D2.position.y -= 25.0
+		$Markers/Marker2D.position.y -=  25.0
+	
+	time_count+=1
+	
+	if time_count < 3:
+		$Timer3.start()
+	else:
+		time_count = -1
+	
+
